960d4207ed40afbc6fa2bf589915ef416d1db5ed
wixette <wixette@gmail.com>
2021-11-10 23:39:18 +0800

    Introduce ISB and support its shell

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,23 +1,75 @@
 using System;
+using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Scripting;
 using ClayConsole;
+using ISB.Runtime;
+using ISB.Utilities;
+
+[Preserve]
+public class Shell
+{
+    [Doc("Print to shell.")]
+    [Preserve]
+    public void Print(StringValue s)
+    {
+        GameManager.Instance.MainConsole.WriteLine(s.ToString());
+    }
+}
 
 public class GameManager : MonoBehaviour {
+  public static GameManager Instance = null;
+
   public MainConsole MainConsole;
 
+  private Engine _engine;
+  private List<string> _multiLineCode;
+  private bool _inMultilineMode => _multiLineCode.Count > 0;
+
+  void Awake() {
+    if (Instance == null) {
+      Instance = this;
+    } else if (Instance != this) {
+      Destroy(gameObject);
+    }
+  }
+
   void Start() {
-    MainConsole.WriteLine("Hello, World!");
+    _engine = new Engine("ClayProgram", new Type[] { typeof(Shell) });
+    _multiLineCode = new List<string>();
+    MainConsole.WriteLine("Welcome to Interactive Small Basic");
     MainConsole.Write("] ");
     MainConsole.StartReadLineLoop(onReadLine);
   }
 
   bool onReadLine(string line) {
-    Debug.Log(line);
-    if (line == "quit") {
-      return false;
+    if (!_inMultilineMode && line == "clear") {
+      _engine.Reset();
+    } else if (!_inMultilineMode && line == "help") {
+      MainConsole.WriteLine(_engine.LibsHelpString);
+    } else if (!_inMultilineMode && line == "list") {
+      MainConsole.WriteLine(string.Join("\n", _engine.CodeLines));
     } else {
-      MainConsole.Write("] ");
-      return true;
+      string code = _inMultilineMode ? string.Join("\n", _multiLineCode) + "\n" + line : line;
+      if (!_engine.Compile(code, false) && _engine.ErrorInfo.Contents.Count > 0) {
+        if (_engine.ErrorInfo.Contents[_engine.ErrorInfo.Contents.Count - 1].Code ==
+            Diagnostic.ErrorCode.UnexpectedEndOfStream) {
+          _multiLineCode.Add(line);
+        } else {
+          MainConsole.WriteLine(_engine.ErrorInfo.Contents[0].ToDisplayString());
+        }
+      } else {
+        _multiLineCode.Clear();
+        if (!_engine.Run(false) && _engine.ErrorInfo.Contents.Count > 0) {
+          MainConsole.WriteLine(_engine.ErrorInfo.Contents[0].ToDisplayString());
+        }
+        if (_engine.StackCount > 0) {
+          BaseValue value = _engine.StackPop();
+          MainConsole.WriteLine(value.ToDisplayString());
+        }
+      }
     }
+    MainConsole.Write(_inMultilineMode ? "> " : "] ");
+    return true;
   }
 }
